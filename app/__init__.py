9937043475d807a3d5ab32579eb98fc7f6e8b07a
Kerice Forde <fordekerice@gmail.com>
2025-04-05 13:35:32 -0500

    Add forms,Fix models,add loginmanager to init

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,17 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db=SQLAlchemy(app)
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 migration= Migrate(app,db)
 from app import views
+from app import models
--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,30 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,FileField,TextAreaField,BooleanField,EmailField,IntegerField,FloatField
+from wtforms.validators import InputRequired,Email
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class Signup(FlaskForm):
+    username=StringField('Username',validators=[InputRequired()])
+    password=PasswordField('Password',validators=[InputRequired()])
+    name=StringField('Name',validator=[InputRequired()])
+    email=EmailField('Email',validator=[InputRequired(),Email])
+    photo=FileField('Photo',validator=[FileRequired(message="File is required!"),FileAllowed(['png','jpeg','jpg'])])
+
+class Login(FlaskForm):
+     username=StringField('Username',validators=[InputRequired()])
+     password=PasswordField('Password',validators=[InputRequired()])
+
+class Profile(FlaskForm):
+     description=TextAreaField('Description',validator=[InputRequired()])
+     parish=StringField('Parish',validators=[InputRequired()])
+     biography=TextAreaField('Biography',validator=[InputRequired()])
+     sex=StringField('Sex',validators=[InputRequired(message="Male or Female")])
+     race=StringField('Race',validators=[InputRequired()])
+     birth_year=IntegerField('Parish',validators=[InputRequired()])
+     height=FloatField('Height',validators=[InputRequired()])
+     fav_cuisine=StringField('FavCuisine',validators=[InputRequired()])
+     fav_school_subject= StringField('FavSchoolSubject',validators=[InputRequired()])
+     political=BooleanField('Political')
+     religion=BooleanField('Religion')
+     family_oriented=BooleanField('Family Oriented')
+     
